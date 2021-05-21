223b84bbb7c72c8c57b7821319b9541d2fb2a244
Erik Dev <Erikss16@live.com>
2021-05-21 17:26:43 -0400

    added find invalid function

--- CreditCardChecker/main.js
+++ CreditCardChecker/main.js
@@ -13,10 +13,10 @@ const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
 const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
 
 // Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]; //
 const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]; //
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]; //
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
 // An array of all the arrays above
@@ -62,10 +62,20 @@ const validateCred = (arr) => {
     //if odd array add begining num that will be skipped
     if (i - 2 < 0 && i != 0) sum += arr[i - 1];
   }
-  console.log(sum);
 
   //if sum % 10 = 0 then valid
   return sum % 10 === 0;
 };
 
-console.log(validateCred(invalid5));
+const findInvalidCards = (arr) => {
+  return arr.filter((card) => !validateCred(card));
+};
+
+//console.log(validateCred(invalid1));
+
+console.log(findInvalidCards(batch));
+
+//testing mystery arr to check if function above works
+//  console.log(validateCred(mystery1));
+//  console.log(validateCred(mystery3));
+//  console.log(validateCred(mystery4));
