410d56318a498458c9211c5b28e600c5b5b2d7f4
Erik Dev <Erikss16@live.com>
2021-07-02 12:55:56 -0400

    Quote project finished

--- quoteAPI/server.js
+++ quoteAPI/server.js
@@ -1,10 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/api/quotes/random", (_req, res) => {
+  const randomQuote = getRandomElement(quotes);
+
+  res.send({
+    quote:  randomQuote 
+  });
+});
+
+app.get("/api/quotes", (req, res) => {
+  const person = req.query.person;
+  const findName = quotes.find((quote) => quote["person"] === person);
+  if (person && findName) {
+    res.send({
+      quotes: [findName],
+    });
+  } else if (person && !findName) {
+    res.send({
+      quotes: [],
+    });
+  } else {
+    res.send({
+      quotes: quotes,
+    });
+  }
+});
+
+app.post("/api/quotes", (req, res) => {
+  const person = req.query.person;
+  const quote = req.query.quote;
+  if (!person || !quote)
+  {
+    res.status(400).send()
+  } else {
+    const newQuote = {
+      quote: quote,
+      person: person
+    }
+    quotes.push(newQuote);
+    res.send({
+      quote: newQuote,
+    });
+    }
+  
+  
+});
+
+
+
+app.listen(PORT);
